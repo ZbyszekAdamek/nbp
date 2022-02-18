e4534148636ac1547a46b430ba5fe2a958ffe34f
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2022-02-18 20:39:57 +0100

    Dodanie niezbędnych metod

--- src/main/java/com/example/nbp/api/NbpClient.java
+++ src/main/java/com/example/nbp/api/NbpClient.java
@@ -3,6 +3,7 @@ package com.example.nbp.api;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Component
@@ -18,14 +19,27 @@ public class NbpClient {
 
     public String getCurrencyForFiveBusinessDays(String currency){
         LocalDate now = LocalDate.now();
-        LocalDate minusFive = now.minusDays(6);
+        /*LocalDate minusFive = now.minusDays(6);*/
+        LocalDate minusFive = subtractDaysSkippingWeekends(now, 4);
         return restTemplate.getForObject(currencyValue
         + "{currency}" + "/" + minusFive + "/" + now, String.class, currency);
     }
 
     public String getAverage(){
         LocalDate now = LocalDate.now();
-        LocalDate minusFourteen = now.minusDays(15);
+        /*LocalDate minusFourteen = now.minusDays(15);*/
+        LocalDate minusFourteen = subtractDaysSkippingWeekends(now, 13);
         return restTemplate.getForObject(average + minusFourteen + "/" + now, String.class);
+    }
+    public LocalDate subtractDaysSkippingWeekends(LocalDate date, int days) {
+        LocalDate result = date;
+        int subtractedDays = 0;
+        while (subtractedDays < days) {
+            result = result.minusDays(1);
+            if (!(result.getDayOfWeek() == DayOfWeek.SATURDAY || result.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+                ++subtractedDays;
+            }
+        }
+        return result;
     }
 }
